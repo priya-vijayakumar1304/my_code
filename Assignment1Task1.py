fd6286d7b86d20fbdb201fbbfbdafb1cc5ba019c
priya-vijayakumar1304 <priya.vijayakumar1304@gmail.com>
2025-06-22 13:02:26 +0530

    Create Assignment1Task1.py

---
+++ Assignment1Task1.py
@@ -0,0 +1,7 @@
+#module2, task1
+First_num=int(input("Enter first number: "))
+Second_num=int(input("Enter second number: "))
+print(f"Addition: {First_num+Second_num}")
+print(f"Subtraction: {First_num-Second_num}")
+print(f"Multiplication: {First_num*Second_num}")
+print(f"Division: {First_num/Second_num}")

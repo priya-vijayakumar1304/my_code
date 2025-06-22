85f96611d635c29f0e0e10d4ddfc37b743489a74
priya-vijayakumar1304 <priya.vijayakumar1304@gmail.com>
2025-06-22 13:04:34 +0530

    Create Assignment1Task2.py

---
+++ Assignment1Task2.py
@@ -0,0 +1,5 @@
+#module2, task 2
+first_name=input("Enter your first name: ")
+last_name=input("Enter your last name: ")
+full_name=first_name+" "+last_name
+print(f"Hello, {full_name}! Welcome to the Python program.")
